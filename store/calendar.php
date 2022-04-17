9d03f2c2e8c6e2e3995669e5a2c2e494966c0fdb
unknown <alexandru0cotofan@gmail.com>
2022-04-17 22:07:11 +0300

    modfied calendar

--- store/calendar.php
+++ store/calendar.php
@@ -16,67 +16,91 @@ include('../auth/server.php');
     <div class="background">
         <div class="calendar">
             <div class="month">
+                <p>Alegeti data si ora programarii:</p>
+            </div>
+            <div class="month">
                 <strong>MAI</strong>
                 <strong>2022</strong>
             </div>
-
             <table>
                 <tr>
-                    <th>Sun</th>
-                    <th>Mon</th>
-                    <th>The</th>
-                    <th>Wed</th>
-                    <th>Thu</th>
-                    <th>Fri</th>
-                    <th>Sat</th>
+                    <th>Dum</th>
+                    <th>Lun</th>
+                    <th>Mar</th>
+                    <th>Mie</th>
+                    <th>Joi</th>
+                    <th>Vin</th>
+                    <th>Sam</th>
                 </tr>
                 <tr>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
-                    <td>5</td>
+                    <td class="available">1</td>
+                    <td class="available">2</td>
+                    <td class="available">3</td>
+                    <td class="available">4</td>
+                    <td class="available">5</td>
                     <td class="not-available">6</td>
-                    <td>7</td>
+                    <td class="available">7</td>
                 </tr>
                 <tr>
-                    <td>8</td>
-                    <td>9</td>
-                    <td>10</td>
+                    <td class="available">8</td>
+                    <td class="available">9</td>
+                    <td class="available">10</td>
                     <td class="not-available">11</td>
-                    <td>12</td>
-                    <td>13</td>
-                    <td>14</td>
+                    <td class="available">12</td>
+                    <td class="available">13</td>
+                    <td class="available">14</td>
                 </tr>
                 <tr>
-                    <td>15</td>
+                    <td class="available">15</td>
                     <td class="not-available">16</td>
-                    <td>17</td>
-                    <td>18</td>
-                    <td>19</td>
+                    <td class="available">17</td>
+                    <td class="available">18</td>
+                    <td class="available">19</td>
                     <td class="not-available">20</td>
-                    <td>21</td>
+                    <td class="available">21</td>
                 </tr>
                 <tr>
-                    <td>22</td>
-                    <td>23</td>
-                    <td>24</td>
-                    <td>25</td>
+                    <td class="available">22</td>
+                    <td class="available">23</td>
+                    <td class="available">24</td>
+                    <td class="available">25</td>
                     <td class="not-available">26</td>
-                    <td>27</td>
-                    <td>28</td>
+                    <td class="available">27</td>
+                    <td class="available">28</td>
                 </tr>
                 <tr>
-                    <td>29</td>
-                    <td>30</td>
-                    <td>31</td>
-                    <td class="no"></td>
-                    <td class="no"></td>
-                    <td class="no"></td>
-                    <td class="no"></td>
+                    <td class="available">29</td>
+                    <td class="available">30</td>
+                    <td class="available">31</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td class="available">8:00 - 9:00</td>
+                    <td class="available">9:00 - 10:00</td>
+                    <td class="available">10:00 - 11:00</td>
+                    <td class="not-available">11:00 - 12:00</td>
+                </tr>
+                <tr>
+                    <td class="not-available">12:00 - 13:00</td>
+                    <td class="available">14:00 - 15:00</td>
+                    <td class="available">15:00 - 16:00</td>
+                    <td class="not-available">16:00 - 17:00</td>
+                </tr>
+                <tr>
+                    <td class="not-available">17:00 - 18:00</td>
+                    <td class="available">18:00 - 19:00</td>
+                    <td class="available">19:00 - 20:00</td>
+                    <td class="not-available">20:00 - 21:00</td>
                 </tr>
             </table>
         </div>
+            
+
 
     </div>
 
