4abbad45fbc758bccd7fd463a37cb02de63012e2
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2022-06-10 00:06:35 +0300

    work

--- admin/stoc.php
+++ admin/stoc.php
@@ -50,8 +50,8 @@ $result = mysqli_query($mysqli, $sql);
     </table>
 
     <div class="buttons">
-        <form method="post" action="stoc.php">
-            <input type="file" class="btn-submit" name="file" accept=".csv,.xls,.xlsx">
+        <form action="stoc.php" method="post" enctype="multipart/form-data">
+            <input type="file" class="btn-submit" name="file" accept=".csv,.xls,.xlsx,.json">
             <input type="submit" class="btn-submit" name="iCSV" value="Import CSV">
             <input type="submit" class="btn-submit" name="iJSON" value="Import JSON">
             <input type="submit" class="btn-submit" name="eCSV" value="Export CSV">
