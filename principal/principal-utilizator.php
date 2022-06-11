7541c9577b56c48305024fd2aec9304ab883edfd
Eusebiu Popescu <popescusebi1234@yahoo.com>
2022-06-11 21:53:26 +0300

    u

--- principal/principal-utilizator.php
+++ principal/principal-utilizator.php
@@ -15,7 +15,7 @@ if (empty($_SESSION['username'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Complete Responsive Website Design</title>
+    <title>Service</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
