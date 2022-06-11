72c7beda0518d24a2957ad5e7e0abe0c779bcaa3
Eusebiu Popescu <popescusebi1234@yahoo.com>
2022-06-11 19:21:49 +0300

    eh

--- admin/raspuns_ajax.php
+++ admin/raspuns_ajax.php
@@ -9,71 +9,53 @@
 </head>
 
 <body>
-  <section class="training__container">
-    <!-- <?php
-      // require_once "functii_php/classment_function.php";
-    ?> -->
-    <table class="training__table">
-      <thead class="training__table__header">
-        <tr>
-          <th>Nume Vehicul</th>
-          <th>Marca</th>
-          <th>Piesa</th>
-          <th>Data</th>
-          <th>Ora</th>
-          <th>Raspuns</th>
-        </tr>
-      </thead>
-      <tbody id="data" class="training__table__body">
-      </tbody>
-    </table>
-  </section>
+<table>
+    <tr>
+    <th>Nume Vehicul</th>
+    <th>Marca</th>
+    <th>Piesa</th>
+    <th>Data</th>
+    <th>Ora</th>
+    <th>Raspuns</th>
+    </tr>
+ 
+    <tbody id="data"></tbody>
+</table>
+ 
+<script>
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", "data.php", true);
+    ajax.send();
+ 
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+ 
+            var html = "";
+            for(var a = 0; a < data.length; a++) {
+                var nume_vehicul = data[a].nume_vehicul;
+                var marca = data[a].marca;
+                var piesa = data[a].piesa;
+                var date = data[a].date;
+                var timeslot = data[a].timeslot;
+                var raspuns = data[a].raspuns;
+ 
+                html += "<tr>";
+                    html += "<td>" + nume_vehicul + "</td>";
+                    html += "<td>" + marca + "</td>";
+                    html += "<td>" + piesa + "</td>";
+                    html += "<td>" + date + "</td>";
+                    html += "<td>" + timeslot + "</td>";
+                    html += "<td>" + raspuns + "</td>";
+                html += "</tr>";
+            }
+            document.getElementById("data").innerHTML += html;
+        }
+    };
+</script>
 
 </body>
 
 </html>
 
-<script>
-  var ajax = new XMLHttpRequest();
-  var method = "GET";
-  var url = "functii_php/data.php";
-  var asynchronous = true;
-
-  ajax.open(method, url, asynchronous);
-  //sendind ajax request
-  ajax.send();
-
-  //receiving response from data.php
-  ajax.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      //converting json back to array
-      var data = JSON.parse(this.responseText);
-      console.log(data); //for debugging
-
-      //html value for <tbody>
-      var html = "";
-      // looping throught the data
-      for (var a = 0; a < data.length; a++) {
-        var nume_vehicul = data[a].nume_vehicul;
-        var marca = data[a].marca;
-        var piesa = data[a].piesa;
-        var data = data[a].data;
-        var ora = data[a].ora;
-        var raspuns = data[a].raspuns;
-
-        //appending  at html
-        html += "<tr>";
-        html += "<td>" + nume_vehicul + "</td>";
-        html += "<td>" + marca + "</td>";
-        html += "<td>" + piesa + "</td>";
-        html += "<td>" + data + "</td>";
-        html += "<td>" + ora + "</td>";
-        html += "<td>" + raspuns + "</td>";
-        html += "</tr>";
-      }
-
-      //replacing <tbody> of <table>
-      document.getElementById("data").innerHTML = html;
-    }
-  }
-</script>
